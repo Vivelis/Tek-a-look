883644021326dcc52cb3112b36b4e51200d753fb
Julien Vignau-Espine <julien.vignau-espine@epitech.eu>
2023-05-25 17:26:20 +0200

    :fix: capital lettersé

--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request):
-    r704 = "704 - c++ room "
-    r705 = "705 - perl room"
-    r706 = "706 - python room"
-    r708 = "708 - english room"
-    r709 = "709 swift room"
-    r710 = "710 - shared room 2"
-    r711 = "711 - haskell room"
-    r616 = "616 - assembly room"
-    r615 = "615 - hub room"
-    r610 = "610 - lean room"
-    r609 = "609 - typescript room"
-    r608 = "608 - dart room"
-    r604 = "604 - kotlin room"
-    r601 = "601 - c room"
+    r704 = "704 - C++ room "
+    r705 = "705 - Perl room"
+    r706 = "706 - Python room"
+    r708 = "708 - English room"
+    r709 = "709 - Swift room"
+    r710 = "710 - Shared room 2"
+    r711 = "711 - Haskell room"
+    r616 = "616 - Assembly room"
+    r615 = "615 - Hub room"
+    r610 = "610 - Lean room"
+    r609 = "609 - Typescript room"
+    r608 = "608 - Dart room"
+    r604 = "604 - Kotlin room"
+    r601 = "601 - C room"
 
     return render(request, 'main.html', {
         'r704': r704, 
